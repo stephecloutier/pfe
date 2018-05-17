81d0b6883509517f2361315b562e13c6adb9e865
Stephe <stephecloutier@gmail.com>
2018-05-17 14:41:18 +0200

    Search bar deployement under 721px

--- static/src/js/script.js
+++ static/src/js/script.js
@@ -1,3 +1,11 @@
+
+const topNavSearchBar = document.getElementById('search-bar')
+const topNavSearchBarLabel = document.getElementById('search-bar-label');
+const topNavSearchButton = document.getElementById('am-search-bar-button');
+const searchInput = document.getElementById('search');
+
+topNavSearchBarLabel.classList.remove('search-bar__label--label-style');
+
 const manageScroll = function() {
     let header = document.getElementById('am-main-bar');
     let topNav = document.getElementById('am-top-nav');
@@ -18,27 +26,39 @@ const manageScroll = function() {
             header.classList.remove('main-bar--sticky');
         }
     }
-
-    
 }
-
-const topNavSearchBar = document.getElementById('search-bar')
-const topNavSearchBarLabel = document.getElementById('search-bar-label');
-
-topNavSearchBarLabel.classList.remove('search-bar__label--label-style');
 
 const moveLabelUp = function() {
     topNavSearchBarLabel.classList.add('search-bar__label--label-style');
 }
 
 const moveLabelDown = function() {
-    const searchInput = document.getElementById('search');
     if(search.value) return;
     topNavSearchBarLabel.classList.remove('search-bar__label--label-style');
     topNavSearchBarLabel.classList.add('search-bar__label--placeholder-style');
 }
 
+const deploySearchBar = function() {
+    topNavSearchBar.classList.add('search-bar--focus');
+    searchInput.focus();
+}
+
+const minimizeSearchBar = function() {
+    if(search.value) return;
+    topNavSearchBar.classList.remove('search-bar--focus');
+}
+
+// events listeners
 
 window.addEventListener('scroll', manageScroll);
-topNavSearchBar.addEventListener('focusin', moveLabelUp);
-topNavSearchBar.addEventListener('focusout', moveLabelDown);
+topNavSearchBar.addEventListener('focusin', function () {
+    moveLabelUp();
+    deploySearchBar();
+});
+
+topNavSearchBar.addEventListener('focusout', function () {
+    moveLabelDown();
+    minimizeSearchBar();
+});
+
+topNavSearchButton.addEventListener('click', deploySearchBar);
