3db2e8f7aa87a16600869713f619e264cfa5a28d
Stephe <stephecloutier@gmail.com>
2018-05-17 22:38:40 +0200

    worked on responsive of homepage

--- static/src/js/script.js
+++ static/src/js/script.js
@@ -6,6 +6,7 @@ const burgerMenu = document.getElementById('am-burger-icon');
 const mainNav = document.getElementById('am-main-nav');
 const closeMenuButton = document.getElementById('am-close-menu');
 const menuBlocker = document.getElementById('am-main-nav-blocker');
+const body = document.getElementsByTagName('body')[0];
 
 topNavSearchBarLabel.classList.remove('search-bar__label--label-style');
 
@@ -13,9 +14,10 @@ const manageScroll = function() {
     let header = document.getElementById('am-main-bar');
     let topNav = document.getElementById('am-top-nav');
     let scrollTop = window.scrollY;
+    //console.log(scrollTop);
 
     let width = window.screen.width;
-
+    //console.log(width);
     if (width <= 720) {
         if(scrollTop >= 70) {
             topNav.classList.add('top-nav--sticky');
@@ -53,10 +55,12 @@ const minimizeSearchBar = function() {
 
 const openMenu = function() {
     mainNav.classList.add('main-nav--opened');
+    body.classList.add('body-with-opened-menu');
 }
 
 const closeMenu = function() {
     mainNav.classList.remove('main-nav--opened');
+    body.classList.remove('body-with-opened-menu');
 }
 
 
